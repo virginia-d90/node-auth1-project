fc3696cc0811ea62e631f6886a6cceb3ed5eba81
Virginia Davenport <virginialdport@gmail.com>
2020-07-20 14:45:47 -0600

    endpoints tested. at MVP

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const KnexSessionStore = require("connect-session-knex")(session);
 
 const usersRouter = require("../users/users-router.js");
 const authRouter = require("../auth/auth-router.js");
-const dbConnection = require("../database/connection.js");
+const dbConnection = require("../data/db-config");
 const authenticate = require("../auth/authenticate-middleware.js");
 
 const server = express();
@@ -33,7 +33,7 @@ const sessionConfiguration = {
 server.use(session(sessionConfiguration)); // enables session support
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+// server.use(cors());
 
 server.use("/api/users", authenticate, usersRouter);
 server.use("/api/auth", authRouter);
